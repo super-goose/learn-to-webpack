48b8bc6c55b76e2db0948358728c842d1433b10c
Jay <theworldneedssupergoose@gmail.com>
2018-01-18 09:13:00 -0600

    Create common.js

---
+++ 000-the-problem/src/common.js
@@ -0,0 +1,16 @@
+export var DefaultZip = (function () {
+    let _thisZip = '00000';
+
+    function _setZip(zip) {
+        _thisZip = zip;
+    }
+
+    function _getZip() {
+        return _thisZip;
+    }
+
+    return {
+        Set: _setZip,
+        Get: _getZip,
+    };
+})();
